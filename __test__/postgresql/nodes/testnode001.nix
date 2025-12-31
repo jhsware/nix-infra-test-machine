38619196fc031f1d0e74d7e66adc3dc1c03ff2b8
Sebastian Ware <sebastian@urbantalk.se>
2025-12-31 09:12:58 +0100

    Fixed postqresql test

--- __test__/postgresql/nodes/testnode001.nix
+++ __test__/postgresql/nodes/testnode001.nix
@@ -1,25 +1,9 @@
 { config, pkgs, lib, ... }: {
-  # Enable PostgreSQL standalone instance using native NixOS service
-  config.services.postgresql = {
+  # Enable PostgreSQL using the infrastructure module
+  infrastructure.postgresql = {
     enable = true;
-    package = pkgs.postgresql_16;
-    enableTCPIP = true;
-    
-    authentication = ''
-      # TYPE  DATABASE        USER            ADDRESS                 METHOD
-      local   all             all                                     trust
-      host    all             all             127.0.0.1/32            trust
-      host    all             all             ::1/128                 trust
-    '';
-
-    settings = {
-      listen_addresses = lib.mkForce "127.0.0.1";
-    };
-
-    # Create initial database
-    ensureDatabases = [ "testdb" ];
+    bindToIp = "127.0.0.1";
+    bindToPort = 5432;
+    initialDatabases = [ "testdb" ];
   };
-
-  # Open firewall for PostgreSQL (only if external access needed)
-  # config.networking.firewall.allowedTCPPorts = [ 5432 ];
 }
