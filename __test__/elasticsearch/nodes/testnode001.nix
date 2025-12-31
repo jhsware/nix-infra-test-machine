a8793500aac573bca39883ba824fbfe868985ff3
Sebastian Ware <sebastian@urbantalk.se>
2025-12-31 09:35:09 +0100

    Fixed elasticsearch

--- __test__/elasticsearch/nodes/testnode001.nix
+++ __test__/elasticsearch/nodes/testnode001.nix
@@ -1,27 +1,12 @@
 { config, pkgs, lib, ... }: {
-  # Enable Elasticsearch standalone instance using native NixOS service
-  config.services.elasticsearch = {
+  # Enable Elasticsearch using the infrastructure module
+  infrastructure.elasticsearch = {
     enable = true;
-    package = pkgs.elasticsearch;
-    dataDir = "/var/lib/elasticsearch";
-    cluster_name = "test-cluster";
-    listenAddress = "127.0.0.1";
-    port = 9202;
-    tcp_port = 9302;
-    single_node = true;
-
-    extraJavaOptions = [
-      "-Xms512m"
-      "-Xmx512m"
-    ];
+    bindToIp = "127.0.0.1";
+    httpPort = 9202;
+    transportPort = 9302;
+    clusterName = "test-cluster";
+    singleNode = true;
+    heapSize = "512m";
   };
-
-  # Install curl and jq for API access
-  config.environment.systemPackages = with pkgs; [
-    curl
-    jq
-  ];
-
-  # Open firewall for Elasticsearch (only if external access needed)
-  # config.networking.firewall.allowedTCPPorts = [ 9202 9302 ];
 }
